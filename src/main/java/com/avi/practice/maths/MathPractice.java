3cf6010275c74222718a4e05e54c92fdf6db65e9
avinashvnera <navinash@vmware.com>
2019-09-15 20:13:31 +0530

    math practice

--- src/main/java/com/avi/practice/maths/MathPractice.java
+++ src/main/java/com/avi/practice/maths/MathPractice.java
@@ -1,6 +1,7 @@
 package com.avi.practice.maths;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -17,9 +18,27 @@ final class MathPractice {
                 factors.add(i);
             }
         }
-        if( A > 2) {
+        if (A > 2) {
             factors.add(A);
         }
+        return factors;
+    }
+
+    final List<Integer> allFactorsOptimized(final int A) {
+        final ArrayList<Integer> factors = new ArrayList<>();
+
+        final double sqrt = Math.sqrt(A);
+        for (int i = 1; i <= sqrt; i++) {
+            if (A % i == 0) {
+                if (A / i == i) {
+                    factors.add(i);
+                } else {
+                    factors.add(i);
+                    factors.add(A / i);
+                }
+            }
+        }
+        Collections.sort(factors);
         return factors;
     }
 }
--- src/test/java/com/avi/practice/maths/MathPracticeTest.java
+++ src/test/java/com/avi/practice/maths/MathPracticeTest.java
@@ -73,4 +73,51 @@ public class MathPracticeTest {
             Assert.assertEquals(expected, tool.allFactors(a));
         }
     }
+
+    @Test
+    public void allFactorsOptimized() {
+        {
+            final int a = 1;
+            final List<Integer> expected = new ArrayList<>();
+            expected.add(1);
+            Assert.assertEquals(expected, tool.allFactorsOptimized(a));
+        }
+        {
+            final int a = 2;
+            final List<Integer> expected = new ArrayList<>();
+            expected.add(1);
+            expected.add(2);
+            Assert.assertEquals(expected, tool.allFactorsOptimized(a));
+        }
+        {
+            final int a = 3;
+            final List<Integer> expected = new ArrayList<>();
+            expected.add(1);
+            expected.add(3);
+            Assert.assertEquals(expected, tool.allFactorsOptimized(a));
+        }
+        {
+            final int a = 6;
+            final List<Integer> expected = new ArrayList<>();
+            expected.add(1);
+            expected.add(2);
+            expected.add(3);
+            expected.add(6);
+            Assert.assertEquals(expected, tool.allFactorsOptimized(a));
+        }
+
+        {
+            final int a = 85463;
+            final List<Integer> expected = new ArrayList<>();
+            expected.add(1);
+            expected.add(7);
+            expected.add(29);
+            expected.add(203);
+            expected.add(421);
+            expected.add(2947);
+            expected.add(12209);
+            expected.add(85463);
+            Assert.assertEquals(expected, tool.allFactorsOptimized(a));
+        }
+    }
 }
