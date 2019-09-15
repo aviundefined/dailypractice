d7c05d9e79b17d878ce08f2123b59f0248b08e4b
avinashvnera <navinash@vmware.com>
2019-09-15 11:27:41 +0530

    practice

---
+++ src/main/java/com/avi/practice/arrays/ArrayPractice.java
@@ -0,0 +1,290 @@
+package com.avi.practice.arrays;
+
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Created by navinash on 14/09/19.
+ * Copyright 2019 VMware, Inc.  All rights reserved.
+ * -- VMware Confidential
+ */
+class ArrayPractice {
+
+    /**
+     * Input:
+     * [ 1, 2, 3 ],
+     * [ 4, 5, 6 ],
+     * [ 7, 8, 9 ],
+     * [ 10, 11, 12 ]
+     * Output:
+     * [1, 2, 3, 6, 5, 4, 7, 8, 9, 12, 11, 10]
+     */
+    final int[] zigZagOrder(final int[][] A) {
+        final int row = A.length;
+        final int col = A[0].length;
+        final int[] zigZag = new int[row * col];
+        int k = 0;
+        for (int i = 0; i < row; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < col; j++) {
+                    zigZag[k] = A[i][j];
+                    k++;
+                }
+            } else {
+                for (int j = col - 1; j >= 0; j--) {
+                    zigZag[k] = A[i][j];
+                    k++;
+                }
+            }
+        }
+        return zigZag;
+    }
+
+    /**
+     * Input:
+     * [ 1, 2, 3 ],
+     * [ 4, 5, 6 ],
+     * [ 7, 8, 9 ]
+     * output:
+     * [1, 2, 3, 6, 9, 8, 7, 4, 5]
+     */
+    final int[] spiralOrder(final int[][] A) {
+        final int row = A.length;
+        final int col = A[0].length;
+        final int[] spiral = new int[row * col];
+        int k = 0;
+        int level = 0;
+        int rowIndex = row - 1;
+        int colIndex = col - 1;
+        while (k < spiral.length) {
+            for (int i = level; i <= colIndex - level; i++) {
+                spiral[k] = A[level][i];
+                k++;
+            }
+
+            if (k < spiral.length) {
+                for (int i = level + 1; i <= rowIndex - 1 - level; i++) {
+                    spiral[k] = A[i][colIndex - level];
+                    k++;
+                }
+            }
+
+            if (k < spiral.length) {
+                for (int i = colIndex - level; i >= level; i--) {
+                    spiral[k] = A[row - 1 - level][i];
+                    k++;
+                }
+            }
+
+            if (k < spiral.length) {
+                for (int i = rowIndex - 1 - level; i >= level + 1; i--) {
+                    spiral[k] = A[i][level];
+                    k++;
+                }
+            }
+            level++;
+        }
+
+        return spiral;
+    }
+
+    /**
+     * Input:
+     * [ 1, 2, 3 ],
+     * [ 4, 5, 6 ],
+     * [ 7, 8, 9 ]
+     * output:
+     * [1, 2, 3, 6, 9, 8, 7, 4, 5]
+     */
+    final int[] spiralOrderApproach2(final int[][] A) {
+        final int R = 0;
+        final int D = 1;
+        final int L = 2;
+        final int U = 3;
+        final int row = A.length;
+        final int col = A[0].length;
+        final int[] spiral = new int[row * col];
+
+        int t = 0, b = row - 1, l = 0, r = col - 1;
+        int dir = 0;
+        int k = 0;
+        while (t <= b && l <= r) {
+            if (dir == R) {
+                for (int i = l; i <= r; i++) {
+                    spiral[k++] = A[t][i];
+                }
+                t++;
+                dir = D;
+            } else if (dir == D) {
+                for (int i = t; i <= b; i++) {
+                    spiral[k++] = A[i][r];
+                }
+                r--;
+                dir = L;
+            } else if (dir == L) {
+                for (int i = r; i >= l; i--) {
+                    spiral[k++] = A[b][i];
+                }
+                b--;
+                dir = U;
+            } else //noinspection ConstantConditions
+                if (dir == U) {
+                    for (int i = b; i >= t; i--) {
+                        spiral[k++] = A[i][l];
+                    }
+                    l++;
+                    dir = R;
+                }
+        }
+        return spiral;
+    }
+
+
+    //    (x+1, y),
+    //    (x - 1, y),
+    //    (x, y+1),
+    //    (x, y-1),
+    //    (x-1, y-1),
+    //    (x+1,y+1),
+    //    (x-1,y+1),
+    //    (x+1,y-1)
+    final int coverPoints(int[] A, int[] B) {
+        if (A.length != B.length) {
+            throw new IllegalArgumentException(String.format("Invalid input: Length of both array should be equal. Length of A : [%d], Length of B : [%d]", A.length, B.length));
+        }
+        // A is X coordinate of i th point
+        // B is Y coordinate of i th point
+        int min = 0;
+        int n = A.length;
+        for (int i = 0; i < n - 1; i++) {
+            final int x = Math.abs(A[i] - A[i + 1]);
+            final int y = Math.abs(B[i] - B[i + 1]);
+            min = min + Math.max(x, y);
+        }
+        return min;
+    }
+
+    final void printArr(final int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (i == arr.length - 1) {
+                System.out.print(arr[i]);
+            } else {
+                System.out.print(arr[i] + ",");
+
+            }
+        }
+        System.out.println();
+    }
+
+
+    final int maxSpecialProduct(int[] A) {
+        final int n = A.length;
+        long max = 0L;
+        for (int i = 1; i <= n - 2; i++) {
+            int rightSplValIdx = Integer.MAX_VALUE;
+            int leftSplValIdx = Integer.MIN_VALUE;
+            // i > j
+            for (int j = i - 1; j >= 0; j--) {
+                if (A[j] > A[i]) {
+                    leftSplValIdx = j;
+                    break;
+                }
+            }
+            // j > i
+            for (int j = i + 1; j <= n - 1; j++) {
+                if (A[j] > A[i]) {
+                    rightSplValIdx = j;
+                    break;
+                }
+            }
+            if (rightSplValIdx != Integer.MAX_VALUE && leftSplValIdx != Integer.MIN_VALUE) {
+                max = Math.max(rightSplValIdx * leftSplValIdx, max);
+            }
+        }
+        return (int) (max % 1000000007);
+    }
+
+    final int maxSpecialProductStack(final int[] A) {
+        int[] left = getLeftSpecialValue(A);
+        int[] right = getRightSpecialValue(A);
+        long max = 0;
+        for (int i = 0; i < A.length; i++) {
+            max = Long.max(max, (long) left[i] * right[i]);
+        }
+        return (int) (max % 1000000007);
+    }
+
+    private int[] getLeftSpecialValue(final int[] a) {
+        final int n = a.length;
+        final int[] leftSplVals = new int[n];
+        final Deque<Integer> q = new ArrayDeque<>();
+        q.add(0);
+        leftSplVals[0] = 0;
+        for (int i = 1; i <= n - 1; i++) {
+            while (!q.isEmpty()) {
+                // if on top of stack found value greater than a[i] then max j found for which a[j] > a[i]
+                if (a[q.getLast()] > a[i]) {
+                    break;
+                }
+                // else remove that index
+                q.removeLast();
+            }
+            leftSplVals[i] = q.isEmpty() ? 0 : q.getLast();
+            q.addLast(i);
+        }
+        return leftSplVals;
+    }
+
+    private int[] getRightSpecialValue(final int[] a) {
+        final int n = a.length;
+        final int[] rightSplVals = new int[n];
+        final Deque<Integer> q = new ArrayDeque<>();
+        q.add(n - 1);
+        rightSplVals[n - 1] = 0;
+        for (int i = n - 2; i >= 0; i--) {
+            while (!q.isEmpty()) {
+                // if on top of stack found greater than a[i] then min index found for which a[j] > a[i]
+                if (a[q.getLast()] > a[i]) {
+                    break;
+                }
+                q.removeLast();
+            }
+            rightSplVals[i] = q.isEmpty() ? 0 : q.getLast();
+            q.addLast(i);
+        }
+        return rightSplVals;
+    }
+
+    final int nobleInteger(int[] A) {
+        final int FOUND = 1;
+        final int NOT_FOUND = -1;
+        Arrays.sort(A);
+        final int n = A.length;
+        // Base case, if last entry after the sorting is 0 then already FOUND
+        if (A[n - 1] == 0) {
+            return FOUND;
+        }
+        // Index i -> Number of integer greater than a[i]
+        final Map<Integer, Integer> map = new LinkedHashMap<>();
+        map.put(n - 1, 0);
+        int lastDuplicate = 0;
+        for (int i = n - 2; i >= 0; i--) {
+            final int last = map.get(i + 1);
+            if (A[i] == A[i + 1]) {
+                map.put(i, last);
+                lastDuplicate++;
+            } else {
+                map.put(i, last + 1 + lastDuplicate);
+                lastDuplicate = 0;
+            }
+            if (map.get(i) == A[i]) {
+                return FOUND;
+            }
+        }
+        return NOT_FOUND;
+    }
+}
---
+++ src/test/java/com/avi/practice/arrays/ArrayPracticeTest.java
@@ -0,0 +1,262 @@
+package com.avi.practice.arrays;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+/**
+ * Created by navinash on 14/09/19.
+ * Copyright 2019 VMware, Inc.  All rights reserved.
+ * -- VMware Confidential
+ */
+public class ArrayPracticeTest {
+
+    private static final int[][] M_3_X_3 = new int[3][3];
+    private static final int[][] M_4_X_4 = new int[4][4];
+    private static final int[][] M_5_X_5 = new int[5][5];
+    private static final int[][] M_4_X_3 = new int[4][3];
+    private static final int[][] M_3_X_4 = new int[3][4];
+    private static final int[][] M_5_X_3 = new int[5][3];
+
+    static {
+        M_3_X_3[0][0] = 1;
+        M_3_X_3[0][1] = 2;
+        M_3_X_3[0][2] = 3;
+        M_3_X_3[1][0] = 4;
+        M_3_X_3[1][1] = 5;
+        M_3_X_3[1][2] = 6;
+        M_3_X_3[2][0] = 7;
+        M_3_X_3[2][1] = 8;
+        M_3_X_3[2][2] = 9;
+
+
+        M_4_X_4[0][0] = 1;
+        M_4_X_4[0][1] = 2;
+        M_4_X_4[0][2] = 3;
+        M_4_X_4[0][3] = 4;
+        M_4_X_4[1][0] = 5;
+        M_4_X_4[1][1] = 6;
+        M_4_X_4[1][2] = 7;
+        M_4_X_4[1][3] = 8;
+        M_4_X_4[2][0] = 9;
+        M_4_X_4[2][1] = 10;
+        M_4_X_4[2][2] = 11;
+        M_4_X_4[2][3] = 12;
+        M_4_X_4[3][0] = 13;
+        M_4_X_4[3][1] = 14;
+        M_4_X_4[3][2] = 15;
+        M_4_X_4[3][3] = 16;
+
+        M_5_X_5[0][0] = 1;
+        M_5_X_5[0][1] = 2;
+        M_5_X_5[0][2] = 3;
+        M_5_X_5[0][3] = 4;
+        M_5_X_5[0][4] = 5;
+        M_5_X_5[1][0] = 6;
+        M_5_X_5[1][1] = 7;
+        M_5_X_5[1][2] = 8;
+        M_5_X_5[1][3] = 9;
+        M_5_X_5[1][4] = 10;
+        M_5_X_5[2][0] = 11;
+        M_5_X_5[2][1] = 12;
+        M_5_X_5[2][2] = 13;
+        M_5_X_5[2][3] = 14;
+        M_5_X_5[2][4] = 15;
+        M_5_X_5[3][0] = 16;
+        M_5_X_5[3][1] = 17;
+        M_5_X_5[3][2] = 18;
+        M_5_X_5[3][3] = 19;
+        M_5_X_5[3][4] = 20;
+        M_5_X_5[4][0] = 21;
+        M_5_X_5[4][1] = 22;
+        M_5_X_5[4][2] = 23;
+        M_5_X_5[4][3] = 24;
+        M_5_X_5[4][4] = 25;
+
+        M_4_X_3[0][0] = 1;
+        M_4_X_3[0][1] = 2;
+        M_4_X_3[0][2] = 3;
+        M_4_X_3[1][0] = 4;
+        M_4_X_3[1][1] = 5;
+        M_4_X_3[1][2] = 6;
+        M_4_X_3[2][0] = 7;
+        M_4_X_3[2][1] = 8;
+        M_4_X_3[2][2] = 9;
+        M_4_X_3[3][0] = 10;
+        M_4_X_3[3][1] = 11;
+        M_4_X_3[3][2] = 12;
+
+        M_3_X_4[0][0] = 1;
+        M_3_X_4[0][1] = 2;
+        M_3_X_4[0][2] = 3;
+        M_3_X_4[0][3] = 4;
+        M_3_X_4[1][0] = 5;
+        M_3_X_4[1][1] = 6;
+        M_3_X_4[1][2] = 7;
+        M_3_X_4[1][3] = 8;
+        M_3_X_4[2][0] = 9;
+        M_3_X_4[2][1] = 10;
+        M_3_X_4[2][2] = 11;
+        M_3_X_4[2][3] = 12;
+
+        M_5_X_3[0][0] = 1;
+        M_5_X_3[0][1] = 2;
+        M_5_X_3[0][2] = 3;
+        M_5_X_3[1][0] = 4;
+        M_5_X_3[1][1] = 5;
+        M_5_X_3[1][2] = 6;
+        M_5_X_3[2][0] = 7;
+        M_5_X_3[2][1] = 8;
+        M_5_X_3[2][2] = 9;
+        M_5_X_3[3][0] = 10;
+        M_5_X_3[3][1] = 11;
+        M_5_X_3[3][2] = 12;
+        M_5_X_3[4][0] = 13;
+        M_5_X_3[4][1] = 14;
+        M_5_X_3[4][2] = 15;
+    }
+
+
+    private ArrayPractice tool;
+
+    @Before
+    public void setup() {
+        tool = new ArrayPractice();
+    }
+
+    @After
+    public void tearDown() {
+        tool = null;
+    }
+
+    @Test
+    public void spiralOrder() {
+        {
+            final int[] spiral = tool.spiralOrder(M_3_X_3);
+            tool.printArr(spiral);
+        }
+        {
+            final int[] spiral = tool.spiralOrder(M_4_X_4);
+            tool.printArr(spiral);
+        }
+        {
+            final int[] spiral = tool.spiralOrder(M_5_X_5);
+            tool.printArr(spiral);
+        }
+        {
+            final int[] spiral = tool.spiralOrder(M_4_X_3);
+            tool.printArr(spiral);
+        }
+        {
+            final int[] spiral = tool.spiralOrder(M_3_X_4);
+            tool.printArr(spiral);
+        }
+        {
+            final int[] spiral = tool.spiralOrder(M_5_X_3);
+            tool.printArr(spiral);
+        }
+    }
+
+    @Test
+    public void spiralOrderApproach2() {
+        {
+            final int[] spiral = tool.spiralOrderApproach2(M_3_X_3);
+            tool.printArr(spiral);
+        }
+        {
+            final int[] spiral = tool.spiralOrderApproach2(M_4_X_4);
+            tool.printArr(spiral);
+        }
+        {
+            final int[] spiral = tool.spiralOrderApproach2(M_5_X_5);
+            tool.printArr(spiral);
+        }
+        {
+            final int[] spiral = tool.spiralOrderApproach2(M_4_X_3);
+            tool.printArr(spiral);
+        }
+        {
+            final int[] spiral = tool.spiralOrderApproach2(M_3_X_4);
+            tool.printArr(spiral);
+        }
+        {
+            final int[] spiral = tool.spiralOrderApproach2(M_5_X_3);
+            tool.printArr(spiral);
+        }
+    }
+
+    @Test
+    public void zigZag() {
+        final ArrayPractice tool = new ArrayPractice();
+        {
+            final int[] zigZag = tool.zigZagOrder(M_3_X_3);
+            tool.printArr(zigZag);
+        }
+        {
+            final int[] zigZag = tool.zigZagOrder(M_4_X_4);
+            tool.printArr(zigZag);
+        }
+        {
+            final int[] zigZag = tool.zigZagOrder(M_5_X_5);
+            tool.printArr(zigZag);
+        }
+        {
+            final int[] zigZag = tool.zigZagOrder(M_4_X_3);
+            tool.printArr(zigZag);
+        }
+        {
+            final int[] zigZag = tool.zigZagOrder(M_3_X_4);
+            tool.printArr(zigZag);
+        }
+        {
+            final int[] zigZag = tool.zigZagOrder(M_5_X_3);
+            tool.printArr(zigZag);
+        }
+    }
+
+    @Test
+    public void coverPoints() {
+        {
+            int[] a = {0, 1, 1};
+            int[] b = {0, 1, 2};
+            Assert.assertEquals(2, tool.coverPoints(a, b));
+        }
+
+
+        {
+            int[] a = {0, 1, 1};
+            int[] b = {0, 1, 2, 4};
+            assertThatThrownBy(() -> tool.coverPoints(a, b))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(String.format("Invalid input: Length of both array should be equal. Length of A : [%d], Length of B : [%d]", a.length, b.length))
+                    .hasNoCause();
+        }
+
+    }
+
+    @Test
+    public void maxSpecialProduct() {
+        final int[] a = {5, 9, 6, 8, 6, 4, 6, 9, 5, 4, 9};
+        Assert.assertEquals(80, tool.maxSpecialProduct(a));
+        Assert.assertEquals(80, tool.maxSpecialProductStack(a));
+    }
+
+    @Test
+    public void nobleInteger() {
+        {
+            final int[] a = {5, 6, 2};
+            Assert.assertEquals(1, tool.nobleInteger(a));
+        }
+        {
+            final int[] a = {1, 2, 7, 0, 9, 3, 6, 0, 6};
+            Assert.assertEquals(-1, tool.nobleInteger(a));
+        }
+        {
+            final int[] a = {-4, -2, 0, -1, -6};
+            Assert.assertEquals(1, tool.nobleInteger(a));
+        }
+    }
+}
