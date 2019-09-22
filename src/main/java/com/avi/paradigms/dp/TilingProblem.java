080042ab009b0badea446986011095330b9cd026
avinashvnera <navinash@vmware.com>
2019-09-23 01:34:54 +0530

    math practice

---
+++ src/main/java/com/avi/paradigms/dp/TilingProblem.java
@@ -0,0 +1,35 @@
+package com.avi.paradigms.dp;
+
+/**
+ * Created by navinash on 23/09/19.
+ * Copyright 2019 VMware, Inc.  All rights reserved.
+ * -- VMware Confidential
+ */
+final class TilingProblem {
+
+    final int numWays(final int n, final int m) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n >= 1 && n < m) {
+            return 1;
+        }
+        if (n == m) {
+            return 2;
+        }
+
+        final int[] dp = new int[n + 1];
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i < m) {
+                dp[i] = 1;
+            } else if (i == m) {
+                dp[i] = 2;
+            } else if (i > m) {
+                dp[i] = dp[i - 1] + dp[i - m];
+            }
+        }
+        return dp[n];
+    }
+}
---
+++ src/test/java/com/avi/paradigms/dp/TilingProblemTest.java
@@ -0,0 +1,24 @@
+package com.avi.paradigms.dp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by navinash on 23/09/19.
+ * Copyright 2019 VMware, Inc.  All rights reserved.
+ * -- VMware Confidential
+ */
+public class TilingProblemTest {
+
+    @Test
+    public void numWays() {
+        final TilingProblem solver = new TilingProblem();
+        {
+            final int n = 7;
+            final int m = 4;
+            final int expected = 5;
+            final int numWays = solver.numWays(n, m);
+            Assert.assertEquals(String.format("%d * %d", n, m), expected, numWays);
+        }
+    }
+}
