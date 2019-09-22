6c9ba196e02fc225846289ed8333c1f72939e59d
avinashvnera <navinash@vmware.com>
2019-09-23 01:35:19 +0530

    math practice

--- src/main/java/com/avi/paradigms/dp/TilingProblem.java
+++ src/main/java/com/avi/paradigms/dp/TilingProblem.java
@@ -17,10 +17,8 @@ final class TilingProblem {
         if (n == m) {
             return 2;
         }
-
         final int[] dp = new int[n + 1];
         dp[0] = 0;
-
         for (int i = 1; i <= n; i++) {
             if (i < m) {
                 dp[i] = 1;
