29a685ea4b396ca2c30e0657ed39cd6d1a2b8630
avinashvnera <navinash@vmware.com>
2019-09-26 08:55:41 +0530

    math practice

--- src/main/java/com/avi/paradigms/dp/FriendsPairing.java
+++ src/main/java/com/avi/paradigms/dp/FriendsPairing.java
@@ -18,7 +18,7 @@ final class FriendsPairing {
         pairs[2] = 2;
         // f(i) is the function to get number of ways to pair ith person, then there are two options
         //  - ith person is single then it's equal to number of ways to pair (i-1) persons i.e. f(i-1)
-        ///  - ith person is not single, then ith person can be paired with (i-1) persons so this will (i-1)*f(i-2)
+        //  - ith person is not single, then ith person can be paired with (i-1) persons so this will (i-1)*f(i-2)
         for (int i = 3; i <= numberOfPeople; i++) {
             pairs[i] = pairs[i - 1] + (i - 1) * pairs[i - 2];
         }
