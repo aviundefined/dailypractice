921f4dee6f42f6502cfb706a5c108fb88af7e716
avinashvnera <navinash@vmware.com>
2019-09-26 13:52:24 +0530

    math practice

---
+++ src/main/java/com/avi/paradigms/dp/SubsetProblem.java
@@ -0,0 +1,49 @@
+package com.avi.paradigms.dp;
+
+import com.avi.practice.utils.CommonUtils;
+
+/**
+ * Created by navinash on 26/09/19.
+ * Copyright 2019 VMware, Inc.  All rights reserved.
+ * -- VMware Confidential
+ */
+final class SubsetProblem {
+
+    private final int[] a;
+    private final int n;
+
+    SubsetProblem(int[] a) {
+        this.a = a;
+        this.n = CommonUtils.isEmpty(a) ? 0 : a.length;
+    }
+
+    /**
+     * Time:  O(n * sum)
+     * Space: O(n * sum)
+     */
+    final boolean isSolutionExists(final int sum) {
+        final boolean[][] dp = new boolean[n + 1][sum + 1];
+        // no elements in array, solution doesn't exist
+        for (int j = 0; j <= sum; j++) {
+            dp[0][j] = false;
+        }
+        // if sum is 0 then solution exists
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = true;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= sum; j++) {
+                // if current sum is equal to or greater than the element at i -1 position then
+                //  - solution can be found with including i -1 element and reducing the sum
+                //  - also excluding the i - 1 element
+                // else only way is also excluding the i - 1 element
+                if (j >= a[i - 1]) {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - a[i - 1]];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+}
---
+++ src/test/java/com/avi/paradigms/dp/SubsetProblemTest.java
@@ -0,0 +1,266 @@
+package com.avi.paradigms.dp;
+
+import com.avi.practice.utils.CommonUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by navinash on 26/09/19.
+ * Copyright 2019 VMware, Inc.  All rights reserved.
+ * -- VMware Confidential
+ */
+public class SubsetProblemTest {
+
+    @Test
+    public void solutionExists() {
+        final int[] a = {3, 34, 4, 12, 5, 2};
+        CommonUtils.printArr(a);
+        final SubsetProblem solver = new SubsetProblem(a);
+        {
+            final int sum = 1;
+            final boolean expected = false;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 2;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 3;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 4;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 5;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 6;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 7;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 8;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 9;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 10;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 11;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 12;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 13;
+            final boolean expected = false;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 14;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 15;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 16;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 17;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 18;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 19;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 20;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 21;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 22;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 23;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 24;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 25;
+            final boolean expected = false;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 26;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 27;
+            final boolean expected = false;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 28;
+            final boolean expected = false;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 29;
+            final boolean expected = false;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 30;
+            final boolean expected = false;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 31;
+            final boolean expected = false;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 32;
+            final boolean expected = false;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 33;
+            final boolean expected = false;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 34;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 35;
+            final boolean expected = false;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 36;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 37;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 38;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 60;
+            final boolean expected = true;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 61;
+            final boolean expected = false;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+        {
+            final int sum = 62;
+            final boolean expected = false;
+            final boolean solutionExists = solver.isSolutionExists(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, solutionExists);
+        }
+    }
+}
