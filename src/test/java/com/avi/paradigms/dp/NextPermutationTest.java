79f004cc59b52addcf7292f4acbc0075709c2409
avinashvnera <navinash@vmware.com>
2019-10-11 19:40:27 +0530

    math practice

--- src/test/java/com/avi/paradigms/dp/NextPermutationTest.java
+++ src/test/java/com/avi/paradigms/dp/NextPermutationTest.java
@@ -1,5 +1,6 @@
 package com.avi.paradigms.dp;
 
+import com.avi.practice.utils.CommonUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -42,5 +43,17 @@ public class NextPermutationTest {
         expected = "6230145";
         next = solver.next(s);
         Assert.assertEquals(s, expected, next);
+    }
+
+    @Test
+    public void all() {
+        final NextPermutation solver = new NextPermutation();
+        String s = "12345";
+        System.out.println(s);
+        while (!CommonUtils.isEmpty(s)) {
+            final String next = solver.next(s);
+            System.out.println(next);
+            s = next;
+        }
     }
 }
