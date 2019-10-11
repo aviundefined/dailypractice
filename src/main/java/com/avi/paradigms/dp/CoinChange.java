72a96b1ec9b52ee17941f7782962dbd113645d66
avinashvnera <navinash@vmware.com>
2019-10-11 20:16:53 +0530

    math practice

--- src/main/java/com/avi/paradigms/dp/CoinChange.java
+++ src/main/java/com/avi/paradigms/dp/CoinChange.java
@@ -2,6 +2,8 @@ package com.avi.paradigms.dp;
 
 import com.avi.practice.utils.CommonUtils;
 
+import java.util.Arrays;
+
 /**
  * Created by navinash on 24/09/19.
  * Copyright 2019 VMware, Inc.  All rights reserved.
@@ -38,5 +40,25 @@ final class CoinChange {
             }
         }
         return change[m][sum];
+    }
+
+    final int minNumberOfCoinsForChange(final int sum) {
+        if (sum == 0) {
+            return 0;
+        }
+        final int[] dp = new int[sum + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= sum; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i - coins[j] >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]]);
+                }
+            }
+            if (dp[i] != Integer.MAX_VALUE) {
+                dp[i] = 1 + dp[i];
+            }
+        }
+        return dp[sum];
     }
 }
--- src/test/java/com/avi/paradigms/dp/CoinChangeTest.java
+++ src/test/java/com/avi/paradigms/dp/CoinChangeTest.java
@@ -61,4 +61,83 @@ public class CoinChangeTest {
             }
         }
     }
+
+    @Test
+    public void minNumberOfCoinsForChange() {
+        final int[] coins = {1, 2, 5};
+        CommonUtils.printArr(coins);
+        final CoinChange solver = new CoinChange(coins);
+        {
+            final int sum = 0;
+            final int expected = 0;
+            final int actual = solver.minNumberOfCoinsForChange(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, actual);
+        }
+        {
+            final int sum = 1;
+            final int expected = 1;
+            final int actual = solver.minNumberOfCoinsForChange(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, actual);
+        }
+        {
+            final int sum = 2;
+            final int expected = 1;
+            final int actual = solver.minNumberOfCoinsForChange(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, actual);
+        }
+        {
+            final int sum = 3;
+            final int expected = 2;
+            final int actual = solver.minNumberOfCoinsForChange(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, actual);
+        }
+        {
+            final int sum = 4;
+            final int expected = 2;
+            final int actual = solver.minNumberOfCoinsForChange(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, actual);
+        }
+        {
+            final int sum = 5;
+            final int expected = 1;
+            final int actual = solver.minNumberOfCoinsForChange(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, actual);
+        }
+        {
+            final int sum = 6;
+            final int expected = 2;
+            final int actual = solver.minNumberOfCoinsForChange(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, actual);
+        }
+        {
+            final int sum = 7;
+            final int expected = 2;
+            final int actual = solver.minNumberOfCoinsForChange(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, actual);
+        }
+        {
+            final int sum = 8;
+            final int expected = 3;
+            final int actual = solver.minNumberOfCoinsForChange(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, actual);
+        }
+        {
+            final int sum = 9;
+            final int expected = 3;
+            final int actual = solver.minNumberOfCoinsForChange(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, actual);
+        }
+        {
+            final int sum = 10;
+            final int expected = 2;
+            final int actual = solver.minNumberOfCoinsForChange(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, actual);
+        }
+        {
+            final int sum = 11;
+            final int expected = 3;
+            final int actual = solver.minNumberOfCoinsForChange(sum);
+            Assert.assertEquals(String.valueOf(sum), expected, actual);
+        }
+    }
 }
