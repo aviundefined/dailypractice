4cdf2773e1c2e1fd4843e3f5b81323472329c2d6
avinashvnera <navinash@vmware.com>
2019-10-13 09:13:46 +0530

    math practice

--- src/main/java/com/avi/practice/strings/StringsPractice.java
+++ src/main/java/com/avi/practice/strings/StringsPractice.java
@@ -1,7 +1,16 @@
 package com.avi.practice.strings;
 
+import com.avi.practice.utils.CommonUtils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 /**
  * Created by navinash on 11/10/19.
@@ -9,6 +18,49 @@ import java.util.Map;
  * -- VMware Confidential
  */
 final class StringsPractice {
+
+    private static final Set<String> dictionary = new HashSet<>();
+
+    static {
+        dictionary.add("a");
+        dictionary.add("service");
+        dictionary.add("system");
+        dictionary.add("out");
+        dictionary.add("put");
+        dictionary.add("output");
+        dictionary.add("outputs");
+        dictionary.add("is");
+        dictionary.add("invalid");
+        dictionary.add("i");
+        dictionary.add("love");
+        dictionary.add("you");
+        dictionary.add("this");
+        dictionary.add("test");
+        dictionary.add("string");
+        dictionary.add("like");
+        dictionary.add("to");
+        dictionary.add("drink");
+        dictionary.add("tea");
+        dictionary.add("coffee");
+        dictionary.add("bee");
+        dictionary.add("bees");
+        dictionary.add("best");
+        dictionary.add("bear");
+        dictionary.add("beer");
+        dictionary.add("bot");
+        dictionary.add("bottom");
+        dictionary.add("bottoms");
+        dictionary.add("up");
+        dictionary.add("ups");
+        dictionary.add("the");
+        dictionary.add("happy");
+        dictionary.add("dog");
+        dictionary.add("went");
+        dictionary.add("park");
+        dictionary.add("on");
+        dictionary.add("sunny");
+        dictionary.add("day");
+    }
 
     /**
      * Time:  O(length of string)
@@ -58,5 +110,61 @@ final class StringsPractice {
             subStr[i] = chars[sIdx + i];
         }
         return String.valueOf(subStr);
+    }
+
+    static List<String> splitSentence(final String word) {
+        final List<String> sentences = new ArrayList<>();
+        final Queue<String> sentence = new LinkedList<>();
+        _splitSentence(word, 0, sentence, sentences);
+         return sentences;
+    }
+
+    private static String queueToSpaceSeparatedString(Collection<String> sentence) {
+        final StringBuilder sb = new StringBuilder();
+        for (final String s : sentence) {
+            sb.append(s).append(" ");
+        }
+        return sb.toString();
+    }
+
+    private static void _splitSentence(
+            final String word,
+            final int start,
+            final Queue<String> sentence,
+            final List<String> sentences) {
+        if (start == word.length()) {
+            sentences.add(queueToSpaceSeparatedString(sentence));
+            return;
+        }
+        for (int i = start; i <= word.length(); i++) {
+            final String part = word.substring(start, i);
+            if (dictionary.contains(part)) {
+                sentence.add(part);
+                _splitSentence(word, i, sentence, sentences);
+                sentence.poll();
+            }
+        }
+    }
+
+
+    static String getSentence(final String word) {
+        return _getSentence(word, 0);
+    }
+
+    private static String _getSentence(final String str, int currIndex) {
+        if (CommonUtils.isEmpty(str)) {
+            return "";
+        }
+        if (dictionary.contains(str)) {
+            return str;
+        }
+        currIndex++;
+        final String firstPart = str.substring(0, currIndex);
+        final String secondPart = str.substring(currIndex);
+        if (dictionary.contains(firstPart)) {
+            return firstPart + " " + _getSentence(secondPart, 0);
+        } else {
+            return _getSentence(str, currIndex);
+        }
     }
 }
--- src/test/java/com/avi/practice/strings/StringsPracticeTest.java
+++ src/test/java/com/avi/practice/strings/StringsPracticeTest.java
@@ -3,10 +3,12 @@ package com.avi.practice.strings;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
 /**
  * Created by navinash on 11/10/19.
  * Copyright 2019 VMware, Inc.  All rights reserved.
- *    -- VMware Confidential
+ * -- VMware Confidential
  */
 public class StringsPracticeTest {
 
@@ -30,6 +32,43 @@ public class StringsPracticeTest {
             final String subStr = StringsPractice.maximumLengthSubstringWithNoDuplicates(str);
             Assert.assertEquals(str, expected, subStr);
         }
+    }
 
+    @Test
+    public void getSentence() {
+        {
+            final String str = "thehappydogwenttotheparkonasunnyday";
+            final String expected = "the happy dog went to the park on a sunny day";
+            final String sentence = StringsPractice.getSentence(str);
+            Assert.assertEquals(str, expected, sentence);
+        }
+        {
+            final String str = "thisisteststring";
+            final String expected = "this is test string";
+            final String sentence = StringsPractice.getSentence(str);
+            Assert.assertEquals(str, expected, sentence);
+        }
+    }
+
+    @Test
+    public void splitSentence() {
+        {
+            final String str = "thehappydogwenttotheparkonasunnyday";
+            final String expected = "the happy dog went to the park on a sunny day";
+            final int numOfSentences = 1;
+            final List<String> sentences = StringsPractice.splitSentence(str);
+            System.out.println(sentences);
+         //   Assert.assertEquals(str, numOfSentences, sentences.size());
+           // Assert.assertEquals(str, expected, sentences.get(0));
+        }
+        {
+            final String str = "thisisteststring";
+            final String expected = "this is test string";
+            final int numOfSentences = 1;
+            final List<String> sentences = StringsPractice.splitSentence(str);
+            System.out.println(sentences);
+       //     Assert.assertEquals(str, numOfSentences, sentences.size());
+         //   Assert.assertEquals(str, expected, sentences.get(0));
+        }
     }
 }
