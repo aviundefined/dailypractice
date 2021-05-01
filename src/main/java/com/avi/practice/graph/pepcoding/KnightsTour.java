5ae78bc9fde6a9dcef1bd6168d4622c64c2954dd
aviundefined <iitk.avinash.nigam@gmail.com>
2021-05-01 20:01:06 +0530

    graph practice

---
+++ src/main/java/com/avi/practice/graph/pepcoding/KnightsTour.java
@@ -0,0 +1,44 @@
+package com.avi.practice.graph.pepcoding;
+
+import java.util.Arrays;
+
+/**
+ * Created by navinash on 01/05/21.
+ * Copyright 2019 VMware, Inc.  All rights reserved.
+ * -- VMware Confidential
+ */
+public class KnightsTour {
+
+    public void knightsTour(final int[][] chess, int row, int col) {
+        helper(chess, row, col, 1);
+    }
+
+    private void helper(int[][] chess, int row, int col, int move) {
+        if (row < 0 || col < 0 || row >= chess.length || col >= chess[0].length || chess[row][col] > 0) {
+            return;
+        }
+        if (move == chess.length * chess[0].length) {
+            chess[row][col] = move;
+            print(chess);
+            chess[row][col] = 0;
+            return;
+        }
+        chess[row][col] = move;
+        helper(chess, row - 2, col + 1, move + 1);
+        helper(chess, row - 1, col + 2, move + 1);
+        helper(chess, row + 1, col + 2, move + 1);
+        helper(chess, row + 2, col + 1, move + 1);
+        helper(chess, row + 2, col - 1, move + 1);
+        helper(chess, row + 1, col - 2, move + 1);
+        helper(chess, row - 1, col - 2, move + 1);
+        helper(chess, row - 2, col - 1, move + 1);
+        chess[row][col] = 0;
+    }
+
+    private void print(int[][] chess) {
+        for (int[] row : chess) {
+            System.out.println(Arrays.toString(row));
+        }
+        System.out.println("-------------");
+    }
+}
---
+++ src/test/java/com/avi/practice/graph/pepcoding/KnightsTourTest.java
@@ -0,0 +1,62 @@
+package com.avi.practice.graph.pepcoding;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * Created by navinash on 01/05/21.
+ * Copyright 2019 VMware, Inc.  All rights reserved.
+ * -- VMware Confidential
+ */
+public class KnightsTourTest {
+    private static final KnightsTour solver = new KnightsTour();
+
+    @Test
+    public void knightsTour1() {
+        final int n = 5;
+        final int row = 4;
+        final int col = 3;
+        final int[][] chess = chess(n);
+        solver.knightsTour(chess, row, col);
+    }
+
+    @Test
+    public void knightsTour2() {
+        final int n = 5;
+        final int row = 0;
+        final int col = 3;
+        final int[][] chess = chess(n);
+        solver.knightsTour(chess, row, col);
+    }
+
+    @Test
+    public void knightsTour3() {
+        final int n = 5;
+        final int row = 2;
+        final int col = 3;
+        final int[][] chess = chess(n);
+        solver.knightsTour(chess, row, col);
+    }
+
+    @Test
+    public void knightTourAll() {
+        final int n = 5;
+
+        for(int row = 0; row < n; row++) {
+            for(int col = 0; col < n; col++) {
+                System.out.printf("Checking for cell: (%s, %s)\n", row, col);
+                final int[][] chess = chess(n);
+                solver.knightsTour(chess, row, col);
+                System.out.println("---------- End ----------");
+            }
+        }
+    }
+    private static int[][] chess(final int n) {
+        final int[][] chess = new int[n][n];
+        for(final int[] row: chess) {
+            Arrays.fill(row, 0);
+        }
+        return chess;
+    }
+}
