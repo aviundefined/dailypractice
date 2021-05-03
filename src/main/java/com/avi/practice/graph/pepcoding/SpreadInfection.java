6f905b0358606cf4a16361a8fa82b885fd521e7e
aviundefined <iitk.avinash.nigam@gmail.com>
2021-05-03 14:19:05 +0530

    graph practice

---
+++ src/main/java/com/avi/practice/graph/pepcoding/SpreadInfection.java
@@ -0,0 +1,54 @@
+package com.avi.practice.graph.pepcoding;
+
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+/**
+ * Created by navinash on 03/05/21.
+ * Copyright 2019 VMware, Inc.  All rights reserved.
+ * -- VMware Confidential
+ */
+public class SpreadInfection {
+
+    public final int infected(final Graph graph, final int source, final int t) {
+        if (graph == null || t <= 0) {
+            return 0;
+        }
+
+        final Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(source, 1));
+        final Map<Integer, Integer> infectedTimeByNode = new HashMap<>();
+        int infected = 0;
+        while (!q.isEmpty()) {
+            // remove mark* word add*
+            final Pair curr = q.poll();
+            if (infectedTimeByNode.containsKey(curr.node)) {
+                continue;
+            }
+            infectedTimeByNode.put(curr.node, curr.time);
+            if (curr.time > t) {
+                break;
+            }
+            infected++;
+            for (final Graph.Edge e : graph.getEdges(curr.node)) {
+                if (!infectedTimeByNode.containsKey(e.getDst())) {
+                    q.add(new Pair(e.getDst(), curr.time + 1));
+                }
+            }
+        }
+        return infected;
+    }
+
+    private static final class Pair {
+        private final int node;
+        private final int time;
+
+        private Pair(int node, int time) {
+            this.node = node;
+            this.time = time;
+        }
+    }
+}
---
+++ src/test/java/com/avi/practice/graph/pepcoding/SpreadInfectionTest.java
@@ -0,0 +1,130 @@
+package com.avi.practice.graph.pepcoding;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by navinash on 03/05/21.
+ * Copyright 2019 VMware, Inc.  All rights reserved.
+ * -- VMware Confidential
+ */
+public class SpreadInfectionTest {
+
+    private static final SpreadInfection solver = new SpreadInfection();
+
+    @Test
+    public void infected1() {
+        /*   1 ------ 3 ----- 5
+         *   |        |      |  \
+         *   |        |      |   \
+         *   |        |      |    \
+         *   2 ------ 4      6---- 7
+         */
+        final Graph graph = GraphTestUtils.getTestUndirectedGraph1();
+        final int source = 1;
+        final int t = 3;
+        final int expected = 5;
+        final int infected = solver.infected(graph, source, t);
+        Assert.assertEquals(expected, infected);
+    }
+
+    @Test
+    public void infected2() {
+        /*   1 ------ 3 ----- 5
+         *   |        |      |  \
+         *   |        |      |   \
+         *   |        |      |    \
+         *   2 ------ 4      6---- 7
+         */
+        final Graph graph = GraphTestUtils.getTestUndirectedGraph1();
+        final int source = 6;
+        final int t = 3;
+        final int expected = 4;
+        final int infected = solver.infected(graph, source, t);
+        Assert.assertEquals(expected, infected);
+    }
+
+    @Test
+    public void infected3() {
+        /*   1        3       5
+         *   |        |      |  \
+         *   |        |      |   \
+         *   |        |      |    \
+         *   2        4      6---- 7
+         */
+        final Graph graph = GraphTestUtils.getTestUndirectedGraph2();
+        final int source = 1;
+        final int t = 3;
+        final int expected = 2;
+        final int infected = solver.infected(graph, source, t);
+        Assert.assertEquals(expected, infected);
+    }
+
+
+    @Test
+    public void infected4() {
+        /*   1        3       5
+         *   |        |      |  \
+         *   |        |      |   \
+         *   |        |      |    \
+         *   2        4      6---- 7
+         */
+        final Graph graph = GraphTestUtils.getTestUndirectedGraph2();
+        final int source = 6;
+        final int t = 3;
+        final int expected = 3;
+        final int infected = solver.infected(graph, source, t);
+        Assert.assertEquals(expected, infected);
+    }
+
+    @Test
+    public void infected5() {
+        /*   1        3 ----- 5
+         *   |        |       |
+         *   |        |       |
+         *   |        |       |
+         *   2 ------ 4      6---- 7
+         */
+        final Graph graph = GraphTestUtils.getTestUndirectedGraph4();
+        final int source = 1;
+        final int t = 3;
+        final int expected = 3;
+        final int infected = solver.infected(graph, source, t);
+        Assert.assertEquals(expected, infected);
+    }
+
+    @Test
+    public void infected6() {
+        /*   1 ------ 3 ----- 5
+         *   |        |      |  \
+         *   |        |      |   \
+         *   |        |      |    \
+         *   2 ------ 4      6---- 7
+         */
+        final Graph graph = GraphTestUtils.getTestUndirectedGraph1();
+        final int source = 3;
+        final int t = 3;
+        final int expected = 7;
+        final int infected = solver.infected(graph, source, t);
+        Assert.assertEquals(expected, infected);
+    }
+
+
+    @Test
+    public void infected7() {
+        /*   1 ------ 3 ----- 5
+         *   |        |      |  \
+         *   |        |      |   \
+         *   |        |      |    \
+         *   2 ------ 4      6---- 7
+         */
+        final Graph graph = GraphTestUtils.getTestUndirectedGraph1();
+        final int source = 3;
+        final int t = 2;
+        final int expected = 4;
+        final int infected = solver.infected(graph, source, t);
+        Assert.assertEquals(expected, infected);
+    }
+}
