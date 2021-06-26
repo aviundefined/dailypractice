5bce23b61be895feac52ea667ea08f52be96afc1
aviundefined <iitk.avinash.nigam@gmail.com>
2021-06-26 11:47:40 +0530

    practice

---
+++ src/main/java/com/avi/practice/company/google/SentenceScreenFitting.java
@@ -0,0 +1,85 @@
+package com.avi.practice.company.google;
+
+/**
+ * Given a rows x cols screen and a sentence represented as a list of strings, return the number of times the given sentence can be fitted on the screen.
+ * <p>
+ * The order of words in the sentence must remain unchanged, and a word cannot be split into two lines. A single space must separate two consecutive words in a line.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: sentence = ["hello","world"], rows = 2, cols = 8
+ * Output: 1
+ * Explanation:
+ * hello---
+ * world---
+ * The character '-' signifies an empty space on the screen.
+ * Example 2:
+ * <p>
+ * Input: sentence = ["a", "bcd", "e"], rows = 3, cols = 6
+ * Output: 2
+ * Explanation:
+ * a-bcd-
+ * e-a---
+ * bcd-e-
+ * The character '-' signifies an empty space on the screen.
+ * Example 3:
+ * <p>
+ * Input: sentence = ["i","had","apple","pie"], rows = 4, cols = 5
+ * Output: 1
+ * Explanation:
+ * i-had
+ * apple
+ * pie-i
+ * had--
+ * The character '-' signifies an empty space on the screen.
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= sentence.length <= 100
+ * 1 <= sentence[i].length <= 10
+ * sentence[i] consists of lowercase English letters.
+ * 1 <= rows, cols <= 2 * 104
+ */
+public class SentenceScreenFitting {
+
+    public int wordsTyping(String[] sentence, int rows, int cols) {
+
+        // try to fit the words till first word is again at start of the row
+        final int numWords = sentence.length;
+        int rem = cols;
+        int row = 0;
+        int count = 0;
+        int i = 0;
+        boolean found = false;
+        boolean isRepeating = false;
+        while (row < rows) {
+            if (found && row != rows && i == 0 && rem == cols) {
+                isRepeating = true;
+                break;
+            }
+            // try to fit current word in current row
+            if (sentence[i].length() <= rem) {
+                rem = rem - sentence[i].length() - 1; // extra space;
+                i = (i + 1) % numWords;
+                if (i == 0) {
+                    count++;
+                    found = true;
+                }
+            } else {
+                row++;
+                rem = cols;
+            }
+        }
+        if (!isRepeating) {
+            return count;
+        }
+        int numSentence = (rows / row) * count;
+        final int remainingRows = rows % row;
+        return numSentence + wordsTyping(sentence, remainingRows, cols);
+    }
+
+
+}
---
+++ src/test/java/com/avi/practice/company/google/SentenceScreenFittingTest.java
@@ -0,0 +1,80 @@
+package com.avi.practice.company.google;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by navinash on 25/06/21.
+ * Copyright 2019 VMware, Inc.  All rights reserved.
+ * -- VMware Confidential
+ */
+public class SentenceScreenFittingTest {
+
+    private static final SentenceScreenFitting solver = new SentenceScreenFitting();
+
+    @Test
+    public void wordsTyping1() {
+        final String[] sentence = {"hello", "world"};
+        final int rows = 2;
+        final int cols = 8;
+        final int expected =    1;
+        final int actual = solver.wordsTyping(sentence, rows, cols);
+        Assert.assertEquals(String.format("%s, %d, %d", Arrays.toString(sentence), rows, cols), expected, actual);
+    }
+
+
+    @Test
+    public void wordsTyping2() {
+        final String[] sentence = {"a", "bcd", "e"};
+        final int rows = 3;
+        final int cols = 6;
+        final int expected =    2;
+        final int actual = solver.wordsTyping(sentence, rows, cols);
+        Assert.assertEquals(String.format("%s, %d, %d", Arrays.toString(sentence), rows, cols), expected, actual);
+    }
+
+    @Test
+    public void wordsTyping3() {
+        final String[] sentence = {"i","had","apple","pie"};
+        final int rows = 4;
+        final int cols = 5;
+        final int expected =    1;
+        final int actual = solver.wordsTyping(sentence, rows, cols);
+        Assert.assertEquals(String.format("%s, %d, %d", Arrays.toString(sentence), rows, cols), expected, actual);
+    }
+
+    @Test
+    public void wordsTyping4() {
+        final String[] sentence = {"hello", "world"};
+        final int rows = 6;
+        final int cols = 8;
+        final int expected =  3;
+        final int actual = solver.wordsTyping(sentence, rows, cols);
+        Assert.assertEquals(String.format("%s, %d, %d", Arrays.toString(sentence), rows, cols), expected, actual);
+    }
+
+    @Test
+    public void wordsTyping5() {
+        final String[] sentence = {"a","bcd"};
+        final int rows = 20000;
+        final int cols = 20000;
+        final int expected =  66660000;
+        final int actual = solver.wordsTyping(sentence, rows, cols);
+        Assert.assertEquals(String.format("%s, %d, %d", Arrays.toString(sentence), rows, cols), expected, actual);
+    }
+
+
+    @Test
+    public void wordsTyping6() {
+        final String[] sentence = {"f","p", "a"};
+        final int rows = 8;
+        final int cols = 7;
+        final int expected =  10;
+        final int actual = solver.wordsTyping(sentence, rows, cols);
+        Assert.assertEquals(String.format("%s, %d, %d", Arrays.toString(sentence), rows, cols), expected, actual);
+    }
+}
