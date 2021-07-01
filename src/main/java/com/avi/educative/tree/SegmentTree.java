9e91be03fc2a831e9415df42cc3b91d26fe45eee
aviundefined <iitk.avinash.nigam@gmail.com>
2021-07-01 20:37:35 +0530

    basic segment tree

---
+++ src/main/java/com/avi/educative/tree/SegmentTree.java
@@ -0,0 +1,85 @@
+package com.avi.educative.tree;
+
+import java.util.Arrays;
+
+/**
+ * Created by navinash on 01/07/21.
+ * Copyright 2019 VMware, Inc.  All rights reserved.
+ * -- VMware Confidential
+ */
+public class SegmentTree {
+
+    private final int[] st;
+    private final int[] arr;
+    private final int n;
+
+    public SegmentTree(final int[] arr) {
+        this.n = arr.length;
+        this.arr = arr;
+        this.st = new int[4 * n];
+        build(arr, st, 0, n - 1, 0);
+        System.out.println("Arr: " + Arrays.toString(arr));
+        System.out.println("Segment Tree: " + Arrays.toString(st));
+    }
+
+    private void build(int[] arr, int[] st, int start, int end, int segmentTreeIdx) {
+
+        if (start == end) {
+            st[segmentTreeIdx] = arr[start];
+            return;
+        }
+
+        final int middle = start + (end - start) / 2;
+        build(arr, st, start, middle, 2 * segmentTreeIdx + 1);
+        build(arr, st, middle + 1, end, 2 * segmentTreeIdx + 2);
+        st[segmentTreeIdx] = st[2 * segmentTreeIdx + 1] + st[2 * segmentTreeIdx + 2];
+
+    }
+
+    public int getRangeSum(final int qStart, final int qEnd) {
+        return getRangeSum(qStart, qEnd, 0, n - 1, 0);
+    }
+
+    private int getRangeSum(int qStart, int qEnd, int start, int end, int segmentTreeIndex) {
+        // complete overlap
+        if (qStart <= start && qEnd >= end) {
+            return st[segmentTreeIndex];
+        }
+        // no overlap
+        if (qStart > end || qEnd < start) {
+            return 0;
+        }
+
+        final int mid = start + (end - start) / 2;
+        final int left = getRangeSum(qStart, qEnd, start, mid, 2 * segmentTreeIndex + 1);
+        final int right = getRangeSum(qStart, qEnd, mid + 1, end, 2 * segmentTreeIndex + 2);
+        return left + right;
+    }
+
+    public void update(int index, int value) {
+        update(0, n - 1, 0, index, value);
+        System.out.println("Arr: " + Arrays.toString(arr));
+        System.out.println("Segment Tree: " + Arrays.toString(st));
+    }
+
+    private void update(int start, int end, int currSegmentTreeIndex, int index, int value) {
+
+
+        if (start == end) {
+            this.arr[index] = value;
+            this.st[currSegmentTreeIndex] = value;
+            return;
+        }
+
+        final int middle = start + (end - start) / 2;
+        if (index <= middle) {
+            // go left
+            update(start, middle, 2 * currSegmentTreeIndex + 1, index, value);
+        } else {
+            // go right
+            update(middle + 1, end, 2 * currSegmentTreeIndex + 2, index, value);
+        }
+        st[currSegmentTreeIndex] = st[2 * currSegmentTreeIndex + 1] + st[2 * currSegmentTreeIndex + 2];
+    }
+
+}
---
+++ src/test/java/com/avi/educative/tree/SegmentTreeTest.java
@@ -0,0 +1,22 @@
+package com.avi.educative.tree;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by navinash on 01/07/21.
+ * Copyright 2019 VMware, Inc.  All rights reserved.
+ * -- VMware Confidential
+ */
+public class SegmentTreeTest {
+
+    @Test
+    public void test() {
+        // - - - - - - - - 0  1  2  3  4  5
+        final int[] arr = {1, 3, 5, 7, 9, 11};
+        final SegmentTree st = new SegmentTree(arr);
+        Assert.assertEquals(21, st.getRangeSum(2, 4));
+        st.update(3, 15);
+        Assert.assertEquals(29, st.getRangeSum(2, 4));
+    }
+}
