c331a69baaf03df0fa1a31ebb11ee64aea003276
aviundefined <iitk.avinash.nigam@gmail.com>
2021-07-04 13:06:29 +0530

    practice

---
+++ src/main/java/com/avi/pepcoding/graph/additionquestions/CriticalConnectionsNetwork.java
@@ -0,0 +1,77 @@
+package com.avi.pepcoding.graph.additionquestions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by navinash on 04/07/21.
+ * Copyright 2019 VMware, Inc.  All rights reserved.
+ * -- VMware Confidential
+ */
+public class CriticalConnectionsNetwork {
+
+    public List<List<Integer>> criticalConnectionsBruteForce(int n, List<List<Integer>> connections) {
+        if (n == 0) {
+            return Collections.emptyList();
+        }
+        final Graph graph = new Graph();
+        for (final List<Integer> connection : connections) {
+            graph.addEdge(connection.get(0), connection.get(1));
+        }
+
+        final Set<List<Integer>> result = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            final Set<Integer> neighbours = graph.getNeighbours(i);
+            final Set<Integer> visited = new HashSet<>();
+            for (final int neighbour : neighbours) {
+                graph.removeEdge(i, neighbour);
+                dfs(i, graph, visited);
+                graph.addEdge(i, neighbour);
+                if (visited.size() < n) {
+                    if (i > neighbour) {
+                        result.add(Arrays.asList(neighbour, i));
+                    } else {
+                        result.add(Arrays.asList(i, neighbour));
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(result);
+    }
+
+    private void dfs(int node, Graph graph, Set<Integer> visited) {
+        visited.add(node);
+        for (final int neighbour : graph.getNeighbours(node)) {
+            if (visited.contains(neighbour)) {
+                continue;
+            }
+            dfs(neighbour, graph, visited);
+        }
+    }
+
+
+    private static final class Graph {
+        private final Map<Integer, Set<Integer>> adjList = new HashMap<>();
+
+        private void addEdge(final int src, final int dst) {
+            this.adjList.computeIfAbsent(src, k -> new HashSet<>()).add(dst);
+            this.adjList.computeIfAbsent(dst, k -> new HashSet<>()).add(src);
+        }
+
+        private void removeEdge(final int src, final int dst) {
+            this.adjList.getOrDefault(src, new HashSet<>()).remove(dst);
+            this.adjList.getOrDefault(dst, new HashSet<>()).remove(src);
+        }
+
+        private Set<Integer> getNeighbours(final int src) {
+            return new HashSet<>(this.adjList.getOrDefault(src, new HashSet<>()));
+        }
+    }
+
+}
---
+++ src/test/java/com/avi/pepcoding/graph/additionquestions/CriticalConnectionsNetworkTest.java
@@ -0,0 +1,49 @@
+package com.avi.pepcoding.graph.additionquestions;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by navinash on 04/07/21.
+ * Copyright 2019 VMware, Inc.  All rights reserved.
+ * -- VMware Confidential
+ */
+public class CriticalConnectionsNetworkTest {
+
+    @Test
+    public void criticalConnections1() {
+        final CriticalConnectionsNetwork solver = new CriticalConnectionsNetwork();
+        final int n = 4;
+        final List<List<Integer>> connections = new ArrayList<>();
+        connections.add(Arrays.asList(0, 1));
+        connections.add(Arrays.asList(1, 2));
+        connections.add(Arrays.asList(2, 0));
+        connections.add(Arrays.asList(1, 3));
+        final List<List<Integer>> expected = new ArrayList<>();
+        expected.add(Arrays.asList(1, 3));
+        final List<List<Integer>> actual = solver.criticalConnectionsBruteForce(n, connections);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void criticalConnections2() {
+        final CriticalConnectionsNetwork solver = new CriticalConnectionsNetwork();
+        final int n = 5;
+        final List<List<Integer>> connections = new ArrayList<>();
+        connections.add(Arrays.asList(1, 0));
+        connections.add(Arrays.asList(2, 0));
+        connections.add(Arrays.asList(3, 2));
+        connections.add(Arrays.asList(4, 2));
+        connections.add(Arrays.asList(4, 3));
+        connections.add(Arrays.asList(3, 0));
+        connections.add(Arrays.asList(4, 0));
+        final List<List<Integer>> expected = new ArrayList<>();
+        expected.add(Arrays.asList(0, 1));
+        final List<List<Integer>> actual = solver.criticalConnectionsBruteForce(n, connections);
+        Assert.assertEquals(expected, actual);
+    }
+}
